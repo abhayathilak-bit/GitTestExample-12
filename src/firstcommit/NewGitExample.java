7e3936cfb98d99d7c09fcfe6fe9b11cc557ed2c6
abhayathilak <abhayathilak@gmail.com>
2026-07-01 00:29:59 +0300

    updated

--- src/firstcommit/NewGitExample.java
+++ src/firstcommit/NewGitExample.java
@@ -5,6 +5,7 @@ public class NewGitExample {
 	public static void main(String[] args) {
 		System.out.println("FirstCommit");
 		System.out.println("updated code");
+		System.out.println("updated again");
 		}
 
 }
